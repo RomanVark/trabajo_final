24f01609c4351142ae83954972ab12ff47b2cb17
RomanVark <jrromero@uamv.edu.ni>
2025-05-14 17:33:48 -0600

    tercera_version

--- ejercicio3.py
+++ ejercicio3.py
@@ -1,17 +1,26 @@
 #control de ventas en la feria universitaria
-# Definicion de variables
+# Definición de variables
 total_feria = 0
-total_dia = 0
-stands = ["stand1", "stand2", "stand3", "stand4"]#deifinicion de stands
-dias = ["miercoles", "jueves", "viernes"] #dias de la feria
-productos = ["producto1", "producto2", "producto3", "producto4"] #productos a vender
-#ingreso de ventas por stand
-for stand in stands:
-    print(f"Ingrese las ventas del {stand} por dia")
-    for dia in dias:
-        print(f"Ingrese las ventas del {dia}")
-        venta = float(input())
-        total_dia += venta
-        total_feria += venta
-    print(f"Total de ventas del {stand}: {total_dia}")
+stands = ["stand1", "stand2", "stand3", "stand4"]  # definición de stands
+dias = ["miercoles", "jueves", "viernes"]  # días de la feria
+productos = ["producto1", "producto2", "producto3"]  # 3 productos por stand
+
+#ingreso de ventas por dias
+for dia in dias:
+    print(f"ingrese las ventas del {dia}")
     total_dia = 0
+    #ingreso de ventas por stand
+    for stand in stands:
+        print(f"\ningrese las ventas del {stand}")
+        total_stand = 0
+        for producto in productos:
+            venta = float(input(f"ingrese la venta del {producto}:"))
+            total_stand += venta
+        print(f"\ntotal de ventas del {stand}: {total_stand}")
+        total_dia += total_stand
+    print(f"\ntotal de ventas del {dia}: {total_dia}")
+    total_feria += total_dia
+print(f"\ntotal de ventas de la feria: {total_feria}")
+
+
+
