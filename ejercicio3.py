16d16cb1692ad034321e72068f692724f40b8826
RomanVark <jrromero@uamv.edu.ni>
2025-05-14 19:08:53 -0600

    version_final_ejercicio3

--- ejercicio3.py
+++ ejercicio3.py
@@ -7,20 +7,21 @@ productos = ["producto1", "producto2", "producto3"]  # 3 productos por stand
 
 #ingreso de ventas por dias
 for dia in dias:
-    print(f"ingrese las ventas del {dia}")
+    print(f"\ningrese las ventas del {dia}")
     total_dia = 0
     #ingreso de ventas por stand
     for stand in stands:
         print(f"\ningrese las ventas del {stand}")
         total_stand = 0
+        #ingreso de ventas por producto
         for producto in productos:
             venta = float(input(f"ingrese la venta del {producto}:"))
             total_stand += venta
+        #calculo de total por stand
         print(f"\ntotal de ventas del {stand}: {total_stand}")
         total_dia += total_stand
+    #calculo de total por dia
     print(f"\ntotal de ventas del {dia}: {total_dia}")
     total_feria += total_dia
+#calculo de total de la feria
 print(f"\ntotal de ventas de la feria: {total_feria}")
-
-
-
